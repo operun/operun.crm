c872c6de160235ece81f00987237b4e73d87f7ba
Stefan Antonelli <stefan.antonelli@operun.de>
2014-04-03 18:10:30 +0200

    updated type info for account and contact

--- operun/crm/account.py
+++ operun/crm/account.py
@@ -5,6 +5,7 @@ from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.interface import invariant, Invalid
 
 from z3c.form import group, field
+from z3c.relationfield.schema import RelationChoice
 
 from plone.namedfile.interfaces import IImageScaleTraversable
 from plone.namedfile.field import NamedImage, NamedFile
@@ -12,31 +13,28 @@ from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 
 from plone.app.textfield import RichText
 
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
 from plone.supermodel import model
 
-#from z3c.relationfield.schema import RelationList, RelationChoice
-#from plone.formwidget.contenttree import ObjPathSourceBinder
-
 from operun.crm import MessageFactory as _
+from operun.crm.config import ACCOUNT_TYPES
 
 
 class IAccount(model.Schema):
     """ Account Content Type
     """
 
-    # name stored in title
-
-    # description 
-
-    # type
-    # selection from customer, vendor, prospect
-
+    type = schema.Choice(
+            title=_(u"Account Type"),
+            vocabulary=ACCOUNT_TYPES,
+            required=True,
+        )
     
     text = RichText(
             title=_(u"Notes"),
             required=False,
         )
-    
     
     phone = schema.TextLine(
             title=_(u"Phone"),
@@ -53,8 +51,14 @@ class IAccount(model.Schema):
             required=False,
         )
 
-    # billing address
+    billing_address = schema.Text(
+            title=_(u"Billing Address"),
+            required=False,
+        )
     
-    # postal address
+    postal_address = schema.Text(
+            title=_(u"Postal Address"),
+            required=False,
+        )
     
     # related contacts
--- operun/crm/contact.py
+++ operun/crm/contact.py
@@ -1,22 +1,22 @@
 from zope import schema
+
 from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from zope.interface import invariant, Invalid
 
 from z3c.form import group, field
+from z3c.relationfield.schema import RelationChoice
 
 from plone.namedfile.interfaces import IImageScaleTraversable
 from plone.namedfile.field import NamedImage, NamedFile
 from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 
 from plone.app.textfield import RichText
-
+from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.supermodel import model
 
-#from z3c.relationfield.schema import RelationList, RelationChoice
-#from plone.formwidget.contenttree import ObjPathSourceBinder
-
+from operun.crm.account import IAccount
+from operun.crm.config import ACCOUNT_TYPES
 from operun.crm import MessageFactory as _
 
 
@@ -28,15 +28,21 @@ class IContact(model.Schema):
 
     # description maybe faded out 
 
-    # type
-    # selection from customer, vendor, prospect
+    type = schema.Choice(
+            title=_(u"Contact Type"),
+            vocabulary=ACCOUNT_TYPES,
+            required=True,
+        )
 
     # job title works like keywords
-    
-    # related account e.g. some company inc.
+
+    account = RelationChoice(
+        title=_(u"Account"),
+        source=ObjPathSourceBinder(object_provides=IAccount.__identifier__),
+        required=False,
+    )
     
     # department e.g. public relations
-        
     
     phone = schema.TextLine(
             title=_(u"Phone"),
@@ -47,10 +53,16 @@ class IContact(model.Schema):
             title=_(u"Mobile"),
             required=False,
         )
-    
-    # office phone from account
-    
-    # office fax from account
+
+    office_phone = schema.TextLine(
+            title=_(u"Office Phone"),
+            required=False,
+        )
+
+    office_fax = schema.TextLine(
+            title=_(u"Office Fax"),
+            required=False,
+        )
 
     email = schema.TextLine(
             title=_(u"E-Mail"),
