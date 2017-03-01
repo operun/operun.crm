355d912920caebe313507313607d0c26c0c5485f
Jesse Stippel <jesse.stippel@operun.de>
2017-03-01 18:09:18 +0100

    added upgrade step

---
+++ operun/crm/upgrades.py
@@ -0,0 +1,48 @@
+"""Upgrade steps for operun.crm."""
+from plone import api
+import logging
+from Products.CMFCore.utils import getToolByName
+
+default_profile = 'profile-operun.crm:default'
+logger = logging.getLogger("Plone")
+
+
+def upgrade_ct(context):
+    """Update operun.crm Content-Type names to new format."""
+    logger.info("Upgrading operun.crm Content-Type names...")
+    catalog = getToolByName(context, 'portal_catalog')
+
+    type_upgrade = {
+        'operun.crm.account': 'Account',
+        'operun.crm.accounts': 'Accounts',
+        'operun.crm.contact': 'Contact',
+        'operun.crm.contacts': 'Contacts',
+        'operun.crm.files': 'Files',
+        'operun.crm.invoice': 'Invoice',
+        'operun.crm.offer': 'Offer',
+        'operun.crm.todo': 'Todo',
+        'operun.crm.todos': 'Todos',
+    }
+
+    for key in type_upgrade.keys():
+        contents = api.content.find(portal_type=key)
+        for item in catalog():
+            obj = item.getObject()
+            if obj.portal_type == key:
+                logger.info(
+                    "{} in catalog is being updated...".format(key)
+                )
+                obj.portal_type = type_upgrade[key]
+        if contents:
+            for item in contents:
+                logger.info(
+                    "{} in portal is being updated...".format(key)
+                )
+                obj = item.getObject()
+                obj.portal_type = type_upgrade[key]
+        else:
+            logger.info(
+                "{} was not updated since no index or items were present.".format(key)  # noqa
+            )
+
+    context.runImportStepFromProfile(default_profile, 'controlpanel')
