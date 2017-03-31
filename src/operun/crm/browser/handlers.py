de7b5abee45d6d0d3fc0f7ddfe17b273edb99606
Jesse Stippel <jesse.stippel@operun.de>
2017-03-31 15:17:39 +0200

    notes

--- src/operun/crm/browser/handlers.py
+++ src/operun/crm/browser/handlers.py
@@ -2,88 +2,106 @@
 import ldap
 import logging
 
+
 logger = logging.getLogger('Plone')
 
-# LDAP config
-KEYS = {
-    'title': 'cn',
-    'email': 'mail',
-    'firstname': 'givenname',
-    'type': 'objectclass',
-    'password': 'userpassword',
-    'lastname': 'sn',
-    'uid': 'uid',
-}
+# NOTE: Should be set in control-panel
+root_users = 'ou=users,dc=operun,dc=de'
+root_groups = 'ou=groups,dc=operun,dc=de'
 
-users = 'ou=users,dc=operun,dc=de'
-groups = 'ou=groups,dc=operun,dc=de'
+server = '10.0.0.126'
+crm_user = 'uid=crm,ou=services,ou=users,dc=operun,dc=de'
+crm_user_pw = 'operuncrm2017'
 
-admin = 'cn=admin,dc=operun,dc=de'
-password = '12345'
-
-server = 'ldap://10.0.0.126'
-
-# Connect
-con = ldap.initialize(server)
-con.simple_bind_s(admin, password)
-
-# LDAP lists
-available_users = con.search_s(users,
-                               ldap.SCOPE_SUBTREE,
-                               '(objectclass=inetOrgPerson)'
-                               )
-available_groups = con.search_s(groups,
-                                ldap.SCOPE_SUBTREE,
-                                '(objectclass=posixGroup)'
-                                )
+tree = dict()
 
 
-def update_user(contact, event):
+def connect(user=None, password=None, server='0.0.0.0'):
     """
-    Update user attributes in LDAP.
+    Connect to LDAP server.
     """
-    mod_attrs = [
-        (ldap.MOD_REPLACE, KEYS['firstname'], str(contact.firstname)),
-        (ldap.MOD_REPLACE, KEYS['lastname'], str(contact.lastname)),
-    ]
-
+    url = 'ldap://{0}'.format(server)
     try:
-        con.modify_s('cn={0},{1}'.format(str(contact.title), users), mod_attrs)
+        logger.info('Contacting LDAP server...')
+        con = ldap.initialize(url)
+        # Bind to LDAP server with credentials
+        con.simple_bind_s(user, password)
     except ldap.LDAPError:
-        logger.info(
-            '{0} could not be updated, either because the attributes do not exist or the user object has changed.'.format(str(contact.firstname))  # noqa
-        )
+        logger.info('Connection failed to {0}'.format(url))
+    else:
+        logger.info('Connected to {0}'.format(url))
+        # Output LDAP users and groups
+        if root_users:
+            try:
+                available_users = con.search_s(root_users, ldap.SCOPE_SUBTREE, '(objectclass=inetOrgPerson)')  # noqa
+                tree['users'] = available_users
+            except ldap.LDAPError:
+                logger.info('NOTE: User does not have access to the users DN.')
+        if root_groups:
+            try:
+                available_groups = con.search_s(root_groups, ldap.SCOPE_SUBTREE, '(objectclass=posixGroup)')  # noqa
+                tree['groups'] = available_groups
+            except ldap.LDAPError:
+                logger.info('NOTE: User does not have access to the groups DN.')  # noqa
+        return con
 
 
-def add_user(contact, event):
+def assign_attribute(attribute_list=[]):
     """
-    Add user in LDAP.
+    Assign field to LDAP attribute.
     """
-    uid = str(contact.firstname)[0] + str(contact.lastname)
+    # NOTE: Need to store dict somewhere..?
+    attribute_map = dict
+    for plone_field, ldap_attribute in attribute_list:
+        attribute_map[plone_field] = ldap_attribute
+    return attribute_map
 
-    add_record = [
-        (KEYS['title'], str(contact.title)),
-        (KEYS['email'], str(contact.email)),
-        (KEYS['firstname'], str(contact.firstname)),
-        (KEYS['type'], 'inetorgperson'),
-        (KEYS['password'], password),
-        (KEYS['lastname'], str(contact.lastname)),
-        (KEYS['uid'], uid.lower()),
-    ]
 
+def update_user(contact, event, attribute_list=[], branch='ou=customers'):
+    """
+    Update user attributes in LDAP directory.
+    attribute_list == [(attr, val),
+                       (attr, val),
+                       (attr, val),]
+    NOTE: Attribute should be LDAP variant!
+    """
+    con = connect(user=crm_user, password=crm_user_pw, server=server)
+    mod_attrs = []
+    title = contact.title
+    if attribute_list:
+        for attribute, value in attribute_list:
+            mod_attrs.append((ldap.MOD_REPLACE, attribute, value))
     try:
-        con.add_s('cn={0},{1}'.format(str(contact.title), users), add_record)
+        con.modify_s('cn={0}{1},{2}'.format(title, ',{0}'.format(branch), root_users), mod_attrs)  # noqa
     except ldap.LDAPError:
-        logger.info(
-            '{0} already exists in the LDAP directory.'.format(str(contact.title))  # noqa
-        )
+        logger.info('{0} could not be updated.'.format(title))
+    con.unbind()
 
 
-def delete_user(contact, event):
+def add_user(contact, event, attribute_list=[], branch='ou=customers'):
+    """
+    Add user into LDAP directory.
+    attribute_list == [(attr, val),
+                       (attr, val),
+                       (attr, val),]
+    NOTE: Attribute should be LDAP variant!
+    """
+    con = connect(user=crm_user, password=crm_user_pw, server=server)
+    title = contact.title
+    try:
+        con.add_s('cn={0}{1},{2}'.format(title, ',{0}'.format(branch), root_users), attribute_list)  # noqa
+    except ldap.LDAPError:
+        logger.info('{0} could not be added.'.format(title))
+    con.unbind()
+
+
+def delete_user(contact, event, branch='ou=customers'):
     """
     Delete user from LDAP.
     """
-    con.delete('cn={0},{1}'.format(str(contact.title), users))
-    logger.info(
-        '{0} removed from LDAP directory.'.format(str(contact.title))
-    )
+    # NOTE: Should use UID instead of name in-case object changes...
+    con = connect(user=crm_user, password=crm_user_pw, server=server)
+    title = contact.title
+    con.delete('cn={0}{1},{2}'.format(title, ',{0}'.format(branch), root_users))  # noqa
+    logger.info('{0} deleted from LDAP directory.'.format(title))
+    con.unbind()
