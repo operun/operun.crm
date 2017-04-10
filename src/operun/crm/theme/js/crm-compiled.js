783fec7de6320026335cbe369af9aee278aa82df
Jesse Stippel <jesse.stippel@operun.de>
2017-04-10 18:47:04 +0200

    compiled bundle

--- src/operun/crm/theme/js/crm-compiled.js
+++ src/operun/crm/theme/js/crm-compiled.js
@@ -1,2 +1,2 @@
-!function(a){"use strict";a(document).ready(function(){})}(jQuery),define("/Users/stefan/Development/operun.crm/src/operun/crm/theme/js/crm.js",function(){});
+!function(a){"use strict";a(document).ready(function(){})}(jQuery),define("/home/jesse/Development/Plone_Projects/operun.crm/src/operun/crm/theme/js/crm.js",function(){});
 //# sourceMappingURL=crm-compiled.js.map
