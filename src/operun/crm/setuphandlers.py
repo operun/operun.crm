5a216a9b102fb0b144acf305f924b98378c03423
Jesse Stippel <jesse.stippel@operun.de>
2017-04-10 15:40:48 +0200

    set default display_types and included missing content-type files

--- src/operun/crm/setuphandlers.py
+++ src/operun/crm/setuphandlers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
@@ -16,8 +17,18 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    _displayed_types()
 
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+
+def _displayed_types():
+    """
+    Add Training to displayed types.
+    """
+    types = api.portal.get_registry_record('plone.displayed_types')
+    types = types + (u'Accounts', u'Contacts', u'Todos',  u'Files')
+    api.portal.set_registry_record('plone.displayed_types', types)
