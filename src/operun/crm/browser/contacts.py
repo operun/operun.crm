1e6282412d7d41adba76d55d866ec24b161cc622
Jesse Stippel <jesse.stippel@operun.de>
2017-04-20 11:41:34 +0200

    added default base views for contacts and accounts

---
+++ src/operun/crm/browser/contacts.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class ContactsView(BrowserView):
+
+    template = ViewPageTemplateFile('templates/contacts.pt')
+
+    def __call__(self):
+        """
+        Custom contacts view
+        """
+
+        return self.template()
+
+    def get_image(self):
+        """
+        Get object image.
+        """
+        context = self.context
+        request = self.request
+        tag = None
+        if context.image:
+            images_view = api.content.get_view('images', context, request)
+            scale = images_view.scale('image')
+            tag = scale.tag()
+        return tag
