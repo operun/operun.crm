653b701c2a78fef011b00f59be3e1881410127d5
Jesse Stippel <jesse.stippel@operun.de>
2018-04-11 11:09:44 +0200

    added dashboard template

---
+++ src/operun/crm/browser/dashboard.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class DashboardView(BrowserView):
+
+    template = ViewPageTemplateFile('templates/dashboard.pt')
+
+    def __call__(self):
+        return self.template()
+
+    def return_portal_url(self):
+        return api.portal.getSite().absolute_url()
